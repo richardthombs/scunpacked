71c5ade42f34a29155884d68c44c445051a9b0c9
Richard Thombs <richard@gearstone.uk>
2019-12-19 10:52:48 +0000

    more work on comparisons

---
+++ website/src/app/Comparisons.ts
@@ -0,0 +1,114 @@
+import { SCItem } from "./SCItem";
+import { SiPipe } from './si.pipe';
+
+export type FieldValue = number | string | undefined;
+export type FieldScore = { value: number, score: number, best: number, worst: number };
+
+export class ComparisonGroup {
+  title: string = "";
+  fields: ComparisonField[] = [];
+  visibleFn: (items: SCItem[]) => boolean = () => true;
+  collapsed: boolean = false;
+
+  constructor(init?: Partial<ComparisonGroup>) {
+    Object.assign(this, init);
+  }
+}
+
+export class ComparisonField {
+
+  title: string = "";
+  units: string = "";
+  siPrefix: boolean = false;
+  sortDirection: "asc" | "desc" = "asc";
+  decimals: number = 0;
+  valueFn: (item: SCItem) => FieldValue = () => undefined;
+  formatFn: (value: FieldValue) => string | null = this.defaultFormat;
+  linkFn?: (item: SCItem) => string = undefined;
+  compareFn?: (all: SCItem[], item: SCItem) => FieldScore | undefined = this.compareValue;
+  bestValue?: number;
+  worstValue?: number;
+  hidden: boolean = false;
+
+  constructor(init?: Partial<ComparisonField>) {
+    Object.assign(this, init);
+  }
+
+  formattedValue(item: SCItem): string {
+    let v = this.valueFn(item);
+    return this.formatFn(v) || "";
+  }
+
+  formattedCompareValue(all: SCItem[], b: SCItem): string {
+    let diff = this.compareValue(all, b);
+    if (diff === undefined) return "";
+
+    if (diff.score === 1) return "";
+
+    let x: number = diff.value - diff.best;
+
+    let fmt = this.formatFn(x) || "";
+    return x > 0 ? "+" + fmt : fmt;
+  }
+
+  compareValue(all: SCItem[], item: SCItem): FieldScore | undefined {
+    if (!this.bestValue || !this.worstValue) return undefined;
+
+    let itemValue = this.valueFn(item);
+    if (!this.isNumber(itemValue)) return undefined;
+
+    let score = 0;
+    if (this.bestValue === this.worstValue) score = 1;
+    else score = (itemValue - this.worstValue) / (this.bestValue - this.worstValue);
+
+    return { value: itemValue, score: score, best: this.bestValue, worst: this.worstValue };
+  }
+
+  compareClass(all: SCItem[], b: SCItem): string {
+    if (!this.compareFn) return "";
+
+    let diff = this.compareValue(all, b);
+    if (diff === undefined) return "";
+
+    if (diff.score === 0) return "worst";
+    if (diff.score === 1) return "best";
+    if (diff.score >= 0.90) return "best-90"
+    if (diff.score >= 0.80) return "best-80"
+    if (diff.score >= 0.70) return "best-70"
+    if (diff.score >= 0.60) return "best-60"
+    if (diff.score >= 0.50) return "best-50"
+    if (diff.score >= 0.40) return "best-40"
+    if (diff.score >= 0.30) return "best-30"
+    if (diff.score >= 0.20) return "best-20"
+    if (diff.score >= 0.10) return "best-10"
+    if (diff.score >= 0.00) return "best-00"
+
+    return "";
+  }
+
+  private defaultFormat(v: FieldValue) {
+    if (v === undefined) return "";
+
+    let value = v, units = this.units, formatted = v.toString();
+
+    if (this.isNumber(value)) {
+      if (isFinite(value)) {
+        if (this.siPrefix) {
+          let si = SiPipe.siPrefix(value);
+          value = si.value;
+          units = si.prefix + this.units;
+        }
+        formatted = Number(value.toFixed(this.decimals)).toLocaleString();
+      }
+      else formatted = "∞";
+    }
+
+    if (formatted && units) formatted += " " + units;
+
+    return formatted;
+  }
+
+  private isNumber(x: any): x is number {
+    return typeof x === "number";
+  }
+}
--- website/src/app/compare-items-page/compare-items-page.component.ts
+++ website/src/app/compare-items-page/compare-items-page.component.ts
@@ -7,114 +7,7 @@ import { environment } from 'src/environments/environment';
 
 import { SCItem } from '../SCItem';
 import { LocalisationService } from '../localisation.service';
-import { SiPipe } from '../si.pipe';
-
-type FieldValue = number | string | undefined;
-type FieldScore = { value: number, score: number, best: number, worst: number };
-
-class ComparisonField {
-
-  title: string = "";
-  units: string = "";
-  siPrefix: boolean = false;
-  sortDirection: "asc" | "desc" = "asc";
-  decimals: number = 0;
-  valueFn: (item: SCItem) => FieldValue = () => undefined;
-  formatFn: (value: FieldValue) => string | null = this.defaultFormat;
-  linkFn?: (item: SCItem) => string = undefined;
-  compareFn?: (all: SCItem[], item: SCItem) => FieldScore | undefined = this.compareValue;
-  bestValue?: number;
-  worstValue?: number;
-  hidden: boolean = false;
-
-  constructor(init?: Partial<ComparisonField>) {
-    Object.assign(this, init);
-  }
-
-  formattedValue(item: SCItem): string {
-    let v = this.valueFn(item);
-    return this.formatFn(v) || "";
-  }
-
-  formattedCompareValue(all: SCItem[], b: SCItem): string {
-    let diff = this.compareValue(all, b);
-    if (diff === undefined) return "";
-
-    if (diff.score === 1) return "";
-
-    let x: number = diff.value - diff.best;
-
-    let fmt = this.formatFn(x) || "";
-    return x > 0 ? "+" + fmt : fmt;
-  }
-
-  compareValue(all: SCItem[], item: SCItem): FieldScore | undefined {
-    if (!this.bestValue || !this.worstValue) return undefined;
-
-    let itemValue = this.valueFn(item);
-    if (!this.isNumber(itemValue)) return undefined;
-
-    let score = 0;
-    if (this.bestValue === this.worstValue) score = 1;
-    else score = (itemValue - this.worstValue) / (this.bestValue - this.worstValue);
-
-    return { value: itemValue, score: score, best: this.bestValue, worst: this.worstValue };
-  }
-
-  compareClass(all: SCItem[], b: SCItem): string {
-    if (!this.compareFn) return "";
-
-    let diff = this.compareValue(all, b);
-    if (diff === undefined) return "";
-
-    if (diff.score === 0) return "worst";
-    if (diff.score === 1) return "best";
-    if (diff.score >= 0.90) return "best-90"
-    if (diff.score >= 0.80) return "best-80"
-    if (diff.score >= 0.70) return "best-70"
-    if (diff.score >= 0.60) return "best-60"
-    if (diff.score >= 0.50) return "best-50"
-    if (diff.score >= 0.40) return "best-40"
-    if (diff.score >= 0.30) return "best-30"
-    if (diff.score >= 0.20) return "best-20"
-    if (diff.score >= 0.10) return "best-10"
-    if (diff.score >= 0.00) return "best-00"
-
-    return "";
-  }
-
-  private defaultFormat(v: FieldValue) {
-    if (v === undefined) return "";
-
-    let value = v, units = this.units, formatted = v.toString();
-
-    if (this.isNumber(value)) {
-      if (isFinite(value)) {
-        if (this.siPrefix) {
-          let si = SiPipe.siPrefix(value);
-          value = si.value;
-          units = si.prefix + this.units;
-        }
-        formatted = Number(value.toFixed(this.decimals)).toLocaleString();
-      }
-      else formatted = "∞";
-    }
-
-    if (formatted && units) formatted += " " + units;
-
-    return formatted;
-  }
-
-  private isNumber(x: any): x is number {
-    return typeof x === "number";
-  }
-}
-
-type ComparisonGroup = {
-  title: string;
-  fields: ComparisonField[];
-  visibleFn: (items: SCItem[]) => boolean;
-}
+import { ComparisonGroup, ComparisonField } from '../Comparisons';
 
 @Component({
   selector: 'app-compare-items-page',
@@ -124,15 +17,15 @@ type ComparisonGroup = {
 export class CompareItemsPage implements OnInit {
 
   fields: ComparisonGroup[] = [
-    {
+    new ComparisonGroup({
       title: "",
       visibleFn: () => true,
       fields: [
         new ComparisonField({ title: "Name", valueFn: i => this.localisationSvc.getText(i.name, i.className), linkFn: i => `/items/${i.className.toLowerCase()}` }),
         new ComparisonField({ title: "Size", valueFn: i => i.size, formatFn: v => `Size ${v}`, compareFn: undefined }),
       ],
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Quantum speed",
       visibleFn: items => !!_.find(items, i => i.quantumDrive),
       fields: [
@@ -142,8 +35,8 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "P2 acceleration", units: "m/s", siPrefix: true, decimals: 1, valueFn: i => _.get(i, "quantumDrive.params.stageTwoAccelRate"), sortDirection: "desc" }),
         new ComparisonField({ title: "Engage speed", units: "m/s", siPrefix: true, decimals: 1, valueFn: i => _.get(i, "quantumDrive.params.engageSpeed") }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Quantum efficiency",
       visibleFn: items => !!_.find(items, i => i.quantumDrive),
       fields: [
@@ -151,8 +44,8 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "Efficiency", units: "m/l", siPrefix: true, valueFn: i => 1 / i.quantumFuelRequirement, sortDirection: "desc" }),
         new ComparisonField({ title: "Quantum fuel requirement", units: "l/Gm", valueFn: i => i.quantumFuelRequirement * 1e9 }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Shield strength",
       visibleFn: items => !!_.find(items, i => i.shieldGenerator),
       fields: [
@@ -162,20 +55,20 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "Downed delay", decimals: 1, valueFn: i => i.shieldGenerator.DownedRegenDelay }),
         new ComparisonField({ title: "Damaged delay", decimals: 1, valueFn: i => i.shieldGenerator.DamagedRegenDelay }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Shield absorption",
       visibleFn: items => !!_.find(items, i => i.shieldGenerator),
       fields: [
-        new ComparisonField({ title: "Damage type 0", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[0].Max }),
-        new ComparisonField({ title: "Damage type 1", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[1].Max }),
-        new ComparisonField({ title: "Damage type 2", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[2].Max }),
-        new ComparisonField({ title: "Damage type 3", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[3].Max }),
-        new ComparisonField({ title: "Damage type 4", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[4].Max }),
-        new ComparisonField({ title: "Damage type 5", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[5].Max }),
+        new ComparisonField({ title: "Physical", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[0].Max }),
+        new ComparisonField({ title: "Energy", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[1].Max }),
+        new ComparisonField({ title: "Distortion", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[2].Max }),
+        new ComparisonField({ title: "Thermal", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[3].Max }),
+        new ComparisonField({ title: "Biochemical", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[4].Max }),
+        new ComparisonField({ title: "Stun", decimals: 2, valueFn: i => i.shieldGenerator.ShieldAbsorption[5].Max }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Armor shielding",
       visibleFn: items => !!_.find(items, i => !!i.armor),
       fields: [
@@ -183,8 +76,8 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "Electromagnetic multiplier", decimals: 2, valueFn: i => i.armor.signalElectromagnetic }),
         new ComparisonField({ title: "Cross section multiplier", decimals: 2, valueFn: i => i.armor.signalCrossSection }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Armor resistances",
       visibleFn: items => !!_.find(items, i => !!i.armor),
       fields: [
@@ -195,8 +88,8 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "Biochemical", decimals: 2, valueFn: i => i.armor.damageMultiplier.DamageInfo.DamageBiochemical }),
         new ComparisonField({ title: "Stun", decimals: 2, valueFn: i => i.armor.damageMultiplier.DamageInfo.DamageStun }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Cooling",
       visibleFn: items => !!_.find(items, i => !!i.cooler),
       fields: [
@@ -204,8 +97,8 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "IR suppression factor", decimals: 2, valueFn: i => i.cooler.SuppressionIRFactor }),
         new ComparisonField({ title: "Heat suppression factor", decimals: 2, valueFn: i => i.cooler.SuppressionHeatFactor }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "EMP",
       visibleFn: items => !!_.find(items, i => !!i.emp),
       fields: [
@@ -219,8 +112,8 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "Unleash time", units: "s", valueFn: i => i.emp.unleashTime }),
         new ComparisonField({ title: "Cooldown time", units: "s", valueFn: i => i.emp.cooldownTime }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Missle damage",
       visibleFn: items => !!_.find(items, i => !!i.missile),
       fields: [
@@ -231,31 +124,31 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "Biochemical damage", valueFn: i => i.missile.explosionParams.damage[0].DamageBiochemical, sortDirection: "desc" }),
         new ComparisonField({ title: "Stun damage", valueFn: i => i.missile.explosionParams.damage[0].DamageStun, sortDirection: "desc" }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Ammunition",
       visibleFn: items => !!_.find(items, i => !!i.ammoContainer),
       fields: [
         new ComparisonField({ title: "Initial ammo count", valueFn: i => _.get(i, "ammoContainer.initialAmmoCount"), sortDirection: "desc" }),
         new ComparisonField({ title: "Max ammo count", valueFn: i => _.get(i, "ammoContainer.maxAmmoCount"), sortDirection: "desc" }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Power plant",
       visibleFn: items => !!_.find(items, i => i.powerConnection && i.type == "PowerPlant"),
       fields: [
         new ComparisonField({ title: "Power generated", units: "W", valueFn: i => _.get(i, "powerConnection.PowerDraw"), sortDirection: "desc" }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Power usage",
       visibleFn: items => !!_.find(items, i => i.powerConnection && i.type != "PowerPlant"),
       fields: [
         new ComparisonField({ title: "Standby power draw", units: "W", valueFn: i => _.get(i, "powerConnection.PowerBase") }),
         new ComparisonField({ title: "Full power draw", units: "W", valueFn: i => _.get(i, "powerConnection.PowerDraw") }),
       ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Power emissions",
       visibleFn: items => !!_.find(items, i => i.powerConnection),
       fields: [
@@ -263,37 +156,42 @@ export class CompareItemsPage implements OnInit {
         new ComparisonField({ title: "EM at standby", units: "J", valueFn: i => _.get(i, "powerConnection.PowerBase") * _.get(i, "powerConnection.PowerToEM") }),
         new ComparisonField({ title: "EM at full power", units: "J", valueFn: i => _.get(i, "powerConnection.PowerDraw") * _.get(i, "powerConnection.PowerToEM") }),
       ]
-    },
-    {
-      title: "Thermal properties",
-      visibleFn: items => !!_.find(items, i => i.heatConnection),
-      fields: [
-        new ComparisonField({ title: "Standby thermal energy", units: "J", valueFn: i => _.get(i, "heatConnection.ThermalEnergyBase") }),
-        new ComparisonField({ title: "Full thermal energy", units: "J", valueFn: i => _.get(i, "heatConnection.ThermalEnergyDraw") }),
-        new ComparisonField({ title: "Specific heat capacity", units: "J/gC", decimals: 1, valueFn: i => _.get(i, "heatConnection.SpecificHeatCapacity") }),
-        new ComparisonField({ title: "Mass", valueFn: i => _.get(i, "heatConnection.Mass") }),
-        new ComparisonField({ title: "Standby temperature", units: "C", valueFn: i => _.get(i, "heatConnection.ThermalEnergyBase", 0) / (_.get(i, "heatConnection.Mass") * _.get(i, "heatConnection.SpecificHeatCapacity")) }),
-        new ComparisonField({ title: "Full power temperature", units: "C", valueFn: i => _.get(i, "heatConnection.ThermalEnergyDraw", 0) / (_.get(i, "heatConnection.Mass") * _.get(i, "heatConnection.SpecificHeatCapacity")) }),
-        new ComparisonField({ title: "Thermal conductivity", decimals: 2, valueFn: i => _.get(i, "heatConnection.ThermalConductivity") }),
-        new ComparisonField({ title: "Surface area", units: "m2", decimals: 2, valueFn: i => _.get(i, "heatConnection.SurfaceArea") }),
-        new ComparisonField({ title: "Max cooling rate", units: "W", valueFn: i => _.get(i, "heatConnection.MaxCoolingRate"), sortDirection: "desc" }),
-      ]
-    },
-    {
-      title: "Thermal emissions",
-      visibleFn: items => !!_.find(items, i => i.powerConnection),
-      fields: [
-        new ComparisonField({ title: "Temperature to IR ratio", decimals: 1, valueFn: i => _.get(i, "heatConnection.TemperatureToIR") })
-      ]
-    },
-    {
+    }),
+    new ComparisonGroup({
       title: "Durability",
       visibleFn: items => !!_.find(items, i => i.degregation || i.health),
       fields: [
         new ComparisonField({ title: "Hitpoints", units: "HP", valueFn: i => i.health, sortDirection: "desc" }),
         new ComparisonField({ title: "Lifetime", units: "h", decimals: 1, valueFn: i => i.maxLifetime, sortDirection: "desc" }),
       ]
-    }
+    }),
+    new ComparisonGroup({
+      title: "Thermal properties",
+      visibleFn: items => !!_.find(items, i => i.heatConnection),
+      collapsed: true,
+      fields: [
+        new ComparisonField({ title: "Min temperature", units: "C", valueFn: i => _.get(i, "heatConnection.MinTemperature") }),
+        new ComparisonField({ title: "Max temperature", units: "C", valueFn: i => _.get(i, "heatConnection.MaxTemperature") }),
+        new ComparisonField({ title: "Cooling starts at", units: "C", valueFn: i => _.get(i, "heatConnection.StartCoolingTemperature") }),
+        new ComparisonField({ title: "Cooling rate", units: "W", valueFn: i => _.get(i, "heatConnection.MaxCoolingRate"), sortDirection: "desc" }),
+        new ComparisonField({ title: "Misfires start at", units: "C", valueFn: i => _.get(i, "heatConnection.MisfireMinTemperature") }),
+        new ComparisonField({ title: "Misfires peak at", units: "C", valueFn: i => _.get(i, "heatConnection.MisfireMaxTemperature") }),
+        new ComparisonField({ title: "Standby thermal energy", units: "J", valueFn: i => _.get(i, "heatConnection.ThermalEnergyBase") }),
+        new ComparisonField({ title: "Full thermal energy", units: "J", valueFn: i => _.get(i, "heatConnection.ThermalEnergyDraw") }),
+        new ComparisonField({ title: "Specific heat capacity", units: "J/gC", decimals: 1, valueFn: i => _.get(i, "heatConnection.SpecificHeatCapacity") }),
+        new ComparisonField({ title: "Mass", valueFn: i => _.get(i, "heatConnection.Mass") }),
+        new ComparisonField({ title: "Thermal conductivity", decimals: 2, valueFn: i => _.get(i, "heatConnection.ThermalConductivity") }),
+        new ComparisonField({ title: "Surface area", units: "m2", decimals: 2, valueFn: i => _.get(i, "heatConnection.SurfaceArea") }),
+      ]
+    }),
+    new ComparisonGroup({
+      title: "Thermal emissions",
+      visibleFn: items => !!_.find(items, i => i.powerConnection),
+      collapsed: true,
+      fields: [
+        new ComparisonField({ title: "Temperature to IR ratio", decimals: 1, valueFn: i => _.get(i, "heatConnection.TemperatureToIR") })
+      ]
+    })
   ];
 
   items: SCItem[] = [];
@@ -334,6 +232,10 @@ export class CompareItemsPage implements OnInit {
     });
   }
 
+  toggleCollapse(group: ComparisonGroup) {
+    group.collapsed = !group.collapsed;
+  }
+
   sortBy(field: ComparisonField) {
     if (field === this.currentSortField) this.currentSortDirection = this.currentSortDirection == "asc" ? "desc" : "asc";
     else this.currentSortDirection = field.sortDirection;
@@ -344,5 +246,4 @@ export class CompareItemsPage implements OnInit {
   private applySort() {
     this.items = _.orderBy(this.items, [i => this.currentSortField.valueFn(i) || 0], [this.currentSortDirection]);
   }
-
 }
