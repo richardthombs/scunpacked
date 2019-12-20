e3371be8247d74f1ffaa0c2fa4ece58d53d27e30
Richard Thombs <richard@gearstone.uk>
2019-12-20 13:20:58 +0000

    Pisces uses a "skipPart" flag to hide the extra wing guns on the basic C8 model

--- Loader/scdb.Xml/Vehicles/Part.cs
+++ Loader/scdb.Xml/Vehicles/Part.cs
@@ -10,14 +10,16 @@ namespace scdb.Xml.Vehicles
 		public string name;
 
 		[XmlAttribute]
-		// TODO: This should be a double but the 3.8.0 ptu seems to be full of garbage characters
-		public string mass;
+		public string mass; // Because 3.8.0 PTU has got dodgy data in the mass attribute
 
 		[XmlAttribute]
 		public double damageMax;
 
 		[XmlAttribute]
 		public string @class;
+
+		[XmlAttribute]
+		public bool skipPart;
 
 		public Part[] Parts;
 
