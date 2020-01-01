7012d5b89ca20700f210f40a91d6a53ee537e0d1
Richard Thombs <richard@gearstone.uk>
2020-01-01 14:15:13 +0000

    Fancier home page

--- website/src/app/navbar/navbar.component.ts
+++ website/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  @Input()
+  dark: boolean = true;
 
   constructor() { }
 
