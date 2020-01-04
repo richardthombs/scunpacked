a9509aefe1f2ebdf9650620aff266539013e1aee
Richard Thombs <richard@gearstone.uk>
2020-01-04 12:16:15 +0000

    Remove redundant dark mode flag from navbar

--- website/src/app/navbar/navbar.component.ts
+++ website/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,9 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  @Input()
-  dark: boolean = true;
 
   constructor() { }
 
