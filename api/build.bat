54899e6610417abf5735d845f7d03ce113516f22
Richard Thombs <richard@gearstone.uk>
2020-02-14 12:08:32 +0000

    Build and deploy using GitHub Actions

--- api/build.bat
+++ api/build.bat
@@ -2,10 +2,6 @@ set name=gearstone/scdb-api
 set latesttag=%name%:latest
 set versiontag=%name%:%npm_package_version%
 
-rmdir dist /s /q
-mkdir .\dist\json
-xcopy ..\Loader\json .\dist\json /s/e/q/y
-
 docker build -t %versiontag% .
 
 docker tag %versiontag% %latesttag%
